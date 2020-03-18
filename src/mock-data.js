99b8865c4989ea5b37f2829e87d93ee7a1025f5b
Joshua Wilkerson <joshua.wilkerson@gmail.com>
2020-03-18 11:25:56 -0600

    Added images and updated mock data

---
+++ src/mock-data.js
@@ -0,0 +1,323 @@
+let mock = [{
+  "name": "Puma",
+  "shooting-two-percent": 63,
+  "shooting-three-percent": 28,
+  "select-three-point": 83,
+  "steal-block-percent": 7,
+  "id": 1,
+  "image": "puma.jpg"
+}, {
+  "name": "Eagle, bateleur",
+  "shooting-two-percent": 51,
+  "shooting-three-percent": 39,
+  "select-three-point": 59,
+  "steal-block-percent": 13,
+  "id": 2,
+  "image": "eaglebateleur.jpg"
+}, {
+  "name": "Red-winged hawk (unidentified)",
+  "shooting-two-percent": 44,
+  "shooting-three-percent": 28,
+  "select-three-point": 18,
+  "steal-block-percent": 22,
+  "id": 3,
+  "image": "redwingedhawkunidentified.jpg"
+}, {
+  "name": "Gull, dusky",
+  "shooting-two-percent": 43,
+  "shooting-three-percent": 10,
+  "select-three-point": 84,
+  "steal-block-percent": 20,
+  "id": 4,
+  "image": "gulldusky.jpg"
+}, {
+  "name": "Deer, barasingha",
+  "shooting-two-percent": 42,
+  "shooting-three-percent": 35,
+  "select-three-point": 58,
+  "steal-block-percent": 9,
+  "id": 5,
+  "image": "deerbarasingha.jpg"
+}, {
+  "name": "King cormorant",
+  "shooting-two-percent": 34,
+  "shooting-three-percent": 14,
+  "select-three-point": 46,
+  "steal-block-percent": 15,
+  "id": 6,
+  "image": "kingcormorant.jpg"
+}, {
+  "name": "Snake-necked turtle",
+  "shooting-two-percent": 47,
+  "shooting-three-percent": 47,
+  "select-three-point": 28,
+  "steal-block-percent": 20,
+  "id": 7,
+  "image": "snakeneckedturtle.jpg"
+}, {
+  "name": "Red-necked phalarope",
+  "shooting-two-percent": 39,
+  "shooting-three-percent": 34,
+  "select-three-point": 82,
+  "steal-block-percent": 23,
+  "id": 8,
+  "image": "redneckedphalarope.jpg"
+}, {
+  "name": "Llama",
+  "shooting-two-percent": 79,
+  "shooting-three-percent": 45,
+  "select-three-point": 66,
+  "steal-block-percent": 12,
+  "id": 9,
+  "image": "llama.jpg"
+}, {
+  "name": "Brown antechinus",
+  "shooting-two-percent": 42,
+  "shooting-three-percent": 39,
+  "select-three-point": 34,
+  "steal-block-percent": 16,
+  "id": 10,
+  "image": "brownantechinus.jpg"
+}, {
+  "name": "Gecko, bent-toed",
+  "shooting-two-percent": 58,
+  "shooting-three-percent": 15,
+  "select-three-point": 50,
+  "steal-block-percent": 22,
+  "id": 11,
+  "image": "geckobenttoed.jpg"
+}, {
+  "name": "Cormorant, large",
+  "shooting-two-percent": 80,
+  "shooting-three-percent": 28,
+  "select-three-point": 64,
+  "steal-block-percent": 24,
+  "id": 12,
+  "image": "cormorantlarge.jpg"
+}, {
+  "name": "Dama wallaby",
+  "shooting-two-percent": 32,
+  "shooting-three-percent": 12,
+  "select-three-point": 77,
+  "steal-block-percent": 11,
+  "id": 13,
+  "image": "damawallaby.jpg"
+}, {
+  "name": "White-winged dove",
+  "shooting-two-percent": 49,
+  "shooting-three-percent": 13,
+  "select-three-point": 18,
+  "steal-block-percent": 25,
+  "id": 14,
+  "image": "whitewingeddove.jpg"
+}, {
+  "name": "Crab-eating raccoon",
+  "shooting-two-percent": 68,
+  "shooting-three-percent": 50,
+  "select-three-point": 65,
+  "steal-block-percent": 8,
+  "id": 15,
+  "image": "crabeatingraccoon.jpg"
+}, {
+  "name": "Platypus",
+  "shooting-two-percent": 33,
+  "shooting-three-percent": 33,
+  "select-three-point": 61,
+  "steal-block-percent": 13,
+  "id": 16,
+  "image": "platypus.jpg"
+}, {
+  "name": "Little grebe",
+  "shooting-two-percent": 69,
+  "shooting-three-percent": 50,
+  "select-three-point": 90,
+  "steal-block-percent": 20,
+  "id": 17,
+  "image": "littlegrebe.jpg"
+}, {
+  "name": "Pallas's fish eagle",
+  "shooting-two-percent": 55,
+  "shooting-three-percent": 28,
+  "select-three-point": 19,
+  "steal-block-percent": 19,
+  "id": 18,
+  "image": "pallassfisheagle.jpg"
+}, {
+  "name": "Wombat, southern hairy-nosed",
+  "shooting-two-percent": 60,
+  "shooting-three-percent": 20,
+  "select-three-point": 45,
+  "steal-block-percent": 24,
+  "id": 19,
+  "image": "wombatsouthernhairynosed.jpg"
+}, {
+  "name": "Black curlew",
+  "shooting-two-percent": 36,
+  "shooting-three-percent": 22,
+  "select-three-point": 26,
+  "steal-block-percent": 5,
+  "id": 20,
+  "image": "blackcurlew.jpg"
+}, {
+  "name": "Skink, blue-tongued",
+  "shooting-two-percent": 48,
+  "shooting-three-percent": 15,
+  "select-three-point": 31,
+  "steal-block-percent": 9,
+  "id": 21,
+  "image": "skinkbluetongued.jpg"
+}, {
+  "name": "Common palm civet",
+  "shooting-two-percent": 67,
+  "shooting-three-percent": 18,
+  "select-three-point": 53,
+  "steal-block-percent": 19,
+  "id": 22,
+  "image": "commonpalmcivet.jpg"
+}, {
+  "name": "Dragon, asian water",
+  "shooting-two-percent": 69,
+  "shooting-three-percent": 14,
+  "select-three-point": 66,
+  "steal-block-percent": 8,
+  "id": 23,
+  "image": "dragonasianwater.jpg"
+}, {
+  "name": "Swan, trumpeter",
+  "shooting-two-percent": 74,
+  "shooting-three-percent": 30,
+  "select-three-point": 78,
+  "steal-block-percent": 21,
+  "id": 24,
+  "image": "swantrumpeter.jpg"
+}, {
+  "name": "Wallaby, river",
+  "shooting-two-percent": 49,
+  "shooting-three-percent": 34,
+  "select-three-point": 27,
+  "steal-block-percent": 22,
+  "id": 25,
+  "image": "wallabyriver.jpg"
+}, {
+  "name": "Starling, cape",
+  "shooting-two-percent": 74,
+  "shooting-three-percent": 40,
+  "select-three-point": 41,
+  "steal-block-percent": 8,
+  "id": 26,
+  "image": "starlingcape.jpg"
+}, {
+  "name": "Green vine snake",
+  "shooting-two-percent": 58,
+  "shooting-three-percent": 10,
+  "select-three-point": 65,
+  "steal-block-percent": 7,
+  "id": 27,
+  "image": "greenvinesnake.jpg"
+}, {
+  "name": "Tarantula",
+  "shooting-two-percent": 43,
+  "shooting-three-percent": 28,
+  "select-three-point": 63,
+  "steal-block-percent": 13,
+  "id": 28,
+  "image": "tarantula.jpg"
+}, {
+  "name": "Cockatoo, roseate",
+  "shooting-two-percent": 76,
+  "shooting-three-percent": 44,
+  "select-three-point": 72,
+  "steal-block-percent": 11,
+  "id": 29,
+  "image": "cockatooroseate.jpg"
+}, {
+  "name": "Asian red fox",
+  "shooting-two-percent": 30,
+  "shooting-three-percent": 27,
+  "select-three-point": 17,
+  "steal-block-percent": 20,
+  "id": 30,
+  "image": "asianredfox.jpg"
+}, {
+  "name": "Tammar wallaby",
+  "shooting-two-percent": 68,
+  "shooting-three-percent": 50,
+  "select-three-point": 28,
+  "steal-block-percent": 11,
+  "id": 31,
+  "image": "tammarwallaby.jpg"
+}, {
+  "name": "Malachite kingfisher",
+  "shooting-two-percent": 51,
+  "shooting-three-percent": 49,
+  "select-three-point": 10,
+  "steal-block-percent": 24,
+  "id": 32,
+  "image": "malachitekingfisher.jpg"
+}, {
+  "name": "Heron, green-backed",
+  "shooting-two-percent": 34,
+  "shooting-three-percent": 18,
+  "select-three-point": 42,
+  "steal-block-percent": 15,
+  "id": 33,
+  "image": "herongreenbacked.jpg"
+}, {
+  "name": "Cottonmouth",
+  "shooting-two-percent": 74,
+  "shooting-three-percent": 23,
+  "select-three-point": 22,
+  "steal-block-percent": 20,
+  "id": 34,
+  "image": "cottonmouth.jpg"
+}, {
+  "name": "Savannah deer (unidentified)",
+  "shooting-two-percent": 48,
+  "shooting-three-percent": 10,
+  "select-three-point": 22,
+  "steal-block-percent": 10,
+  "id": 35,
+  "image": "savannahdeerunidentified.jpg"
+}, {
+  "name": "European stork",
+  "shooting-two-percent": 60,
+  "shooting-three-percent": 48,
+  "select-three-point": 23,
+  "steal-block-percent": 12,
+  "id": 36,
+  "image": "europeanstork.jpg"
+}, {
+  "name": "Tern, royal",
+  "shooting-two-percent": 69,
+  "shooting-three-percent": 27,
+  "select-three-point": 30,
+  "steal-block-percent": 8,
+  "id": 37,
+  "image": "ternroyal.jpg"
+}, {
+  "name": "Mockingbird, galapagos",
+  "shooting-two-percent": 76,
+  "shooting-three-percent": 34,
+  "select-three-point": 33,
+  "steal-block-percent": 17,
+  "id": 38,
+  "image": "mockingbirdgalapagos.jpg"
+}, {
+  "name": "Bee-eater (unidentified)",
+  "shooting-two-percent": 70,
+  "shooting-three-percent": 25,
+  "select-three-point": 83,
+  "steal-block-percent": 10,
+  "id": 39,
+  "image": "beeeaterunidentified.jpg"
+}, {
+  "name": "COVID-19",
+  "shooting-two-percent": 100,
+  "shooting-three-percent": 100,
+  "select-three-point": 100,
+  "steal-block-percent": 100,
+  "id": 40,
+  "image": "covid19.jpg"
+}]
+
+export default mock;
